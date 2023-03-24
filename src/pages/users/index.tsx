d948ae448b0b30188283eb502e4c167dc4aac520
Vinícios Oliveira <vinicompif@gmail.com>
2023-03-24 02:00:57 -0300

    add axios, create users hook, and refac code

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -19,14 +19,7 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import { SideBar } from "../../components/Sidebar";
-import { useQuery } from "react-query";
-
-type User = {
-  id: string;
-  name: string;
-  email: string;
-  createdAt: string;
-};
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
   const isWideVersion = useBreakpointValue({
@@ -34,28 +27,7 @@ export default function UserList() {
     lg: true,
   });
 
-  const { data, isLoading, error, isFetching } = useQuery<User[]>(
-    "users",
-    async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
-
-      const users = data.users.map((user) => {
-        return {
-          id: user.id,
-          name: user.name,
-          email: user.email,
-          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-            day: "2-digit",
-            month: "long",
-            year: "numeric",
-          }),
-        };
-      });
-
-      return users;
-    }
-  );
+  const { data, isLoading, error, isFetching } = useUsers();
 
   return (
     <Box>
